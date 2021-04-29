280efc92c7eb46ec70602f4ce94898424608325d
Alex <alexdeassis7@gmail.com>
2021-04-29 18:56:08 +0200

    CLase5 - APi rest con Spring Security-JWT-H2-Mysql

---
+++ clase5YeomanRestSpringJWT/src/main/java/com/eduit/clase5YeomanRestSpringJWTApplication.java
@@ -0,0 +1,12 @@
+package com.eduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class clase5YeomanRestSpringJWTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(clase5YeomanRestSpringJWTApplication.class, args);
+	}
+}
